ed482fbf4cecb833e6d46f9d46bba4497b5deef3
MaxGeraedts <mailmaxg@gmail.com>
2025-03-10 22:05:14 +0100

    Added StressTensor calculation

--- Plotting.py
+++ Plotting.py
@@ -150,3 +150,138 @@ def ParticleMesh(data):
         sphere = DeformedParticleMesh(radius,center,ParContactPoints[i],ParContactNormals[i])
         geom.append(sphere)
     return geom
+
+## Plot Vectors
+def Plot3DVectors(origin,direction,BC):
+    from mpl_toolkits.mplot3d import Axes3D
+    X,Y,Z = [origin[:,i] for i in [0,1,2]]
+    U, V, W = [direction[:,i] for i in [0,1,2]]
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.quiver(X, Y, Z, U, V, W)
+    ax.set_xlim([BC[3,0], BC[0,0]])
+    ax.set_ylim([BC[4,1], BC[1,1]])
+    ax.set_zlim([BC[5,2], BC[2,2]])
+    ax.set_aspect('equal')
+    plt.show()
+
+# Evaluation Visualization
+## Get Stress Tensor
+def EffectiveE(data,idx):
+    poisson_ratio = data.x[idx,2]
+    Youngs_mod = data.x[idx,1]
+    return (1-torch.square(poisson_ratio))/Youngs_mod
+    
+def EffectiveStiffness(data):
+    """Calculate relative stiffnes Nij for all contacts
+
+    Args:
+        data (object): Graph describing particle assembly
+
+    Returns:
+        array: 1xNcontact
+    """
+    i,j = data.edge_index[1,:], data.edge_index[0,:]
+    E_ij = EffectiveE(data,i)
+    E_ij[data.edge_mask]+=EffectiveE(data,j[data.edge_mask])
+
+    R_ij = 1/data.x[i,0] 
+    R_ij[data.edge_mask]+=1/data.x[j[data.edge_mask],0]
+
+    Nij = (4/3)*1/(E_ij)*(1/torch.sqrt(R_ij))
+    return Nij
+
+def GetGamma(data):
+    """Calculate relative displacement for all contacts
+
+    Args:
+        data (object): Graph describing particle assembly
+
+    Returns:
+        array: 3xNcontact
+    """
+    i,j = data.edge_index[1,:], data.edge_index[0,:]
+    R_i = data.x[i,0]
+    R_i += data.x[j,0]
+    gamma = R_i - torch.norm(data.pos[i]-data.pos[j],dim=1)
+    gamma[gamma<0] = 0
+    return gamma
+
+def GetContactForce(data):
+    """Calculate contact force vectors for all contacts
+
+    Args:
+        data (object): Graph describing particle assembly
+
+    Returns:
+        array: 3xNcontact array
+    """
+    i,j = data.edge_index[1,:], data.edge_index[0,:]
+    gamma = GetGamma(data)
+    Nij = EffectiveStiffness(data)
+    Fij_size = Nij*(gamma*torch.sqrt(gamma))
+    contactnormal = (data.pos[i]-data.pos[j])
+    contactnormal = torch.div(contactnormal.T,torch.norm(contactnormal,dim=1))
+    Fij = Fij_size*contactnormal
+    return Fij.T
+
+def GetVolume(BC):
+    """Given a boundary condition array at time t, calculate volume
+
+    Args:
+        BC (Array): Boundary condition hyperplane points at timestep
+
+    Returns:
+        float: Boundary volume
+    """
+    xmax, xmin = BC[0,0], BC[3,0]
+    ymax, ymin = BC[1,1], BC[4,1]
+    zmax, zmin = BC[2,2], BC[5,2]
+    vol = (xmax-xmin)*(ymax-ymin)*(zmax-zmin)
+    return vol
+
+def GetStressTensor(data,BC):
+    """Calculate Internal stress tensor for one timestep
+
+    Args:
+        data (Data object): Graph describing particle assembly at time t
+        BC (array): Boundary condition at time t
+
+    Returns:
+        array: 3x3 Gauchy stress tensor
+    """
+    contactpoints = GetAllContactpoints(data)
+    contactvector = contactpoints[data.edge_index[0,:]] - data.pos[data.edge_index[1,:]]
+    contactforce = GetContactForce(data)
+    stress_tensor = torch.zeros((3,3))
+    vol = GetVolume(BC)
+    for contact in range(contactforce.shape[0]):
+        Fij = contactforce[contact].reshape(3,1)
+        Lij = contactvector[contact].reshape(1,3)
+        stress_tensor += Fij*Lij
+    stress_tensor /= vol
+    return stress_tensor
+
+def GetInternalStressRollout(Rollout):
+    """Calculate internal stress tensor (Gauchy) for every timestep
+
+    Args:
+        Rollout (list): List of Torch Data objects describing particle assemblies
+
+    Returns:
+        array: [Timestep[3x3]]
+    """
+    stress_evo = torch.zeros((Rollout.timesteps,3,3))
+    for t in range(Rollout.timesteps):
+        data = Rollout.GroundTruth[t]
+        BC = Rollout.BC_rollout[t][:,:3]
+        stress_evo[t] = GetStressTensor(data,BC)
+    return stress_evo
+
+def PlotContactVectorAndForce(data,BC):
+    contactforce = GetContactForce(data)
+    contactpoints = GetAllContactpoints(data)
+    contactvector = contactpoints[data.edge_index[0,:]] - data.pos[data.edge_index[1,:]]
+    Plot3DVectors(data.pos[data.edge_index[1,:]],contactvector[data.edge_index[0,:]],BC)
+    Plot3DVectors(contactpoints,(contactforce/torch.max(contactforce))/4,BC)
