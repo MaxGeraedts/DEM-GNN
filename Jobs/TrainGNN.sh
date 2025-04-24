3d157a64a6d3bc5039cbd6aa999887dc17cc3e0e
MaxGeraedts <mailmaxg@gmail.com>
2025-04-24 15:34:28 +0200

    Added email notification

--- Jobs/TrainGNN.sh
+++ Jobs/TrainGNN.sh
@@ -7,6 +7,8 @@
 #SBATCH --gpus-per-task=1
 #SBATCH --mem-per-gpu=2gb
 #SBATCH --time=00:05:00
+#SBATCH --mail-user=m.geraedts1@student.tue.nl
+#SBATCH --mail-type=END
 
 module purge
 module load PyTorch/2.1.2-foss-2023a-CUDA-12.1.1
