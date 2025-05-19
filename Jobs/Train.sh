0890e21ddff903e102378daa35d1c60101160681
MaxGeraedts <mailmaxg@gmail.com>
2025-05-19 16:03:58 +0200

    Changed Job script

--- Jobs/Train.sh
+++ Jobs/Train.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=test_python
-#SBATCH --output=test_python-%j.log
+#SBATCH --job-name=ML-Training
+#SBATCH --output=Training-%j.log
 #SBATCH --partition=be.gpuresearch.q
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --gpus-per-task=1
 #SBATCH --mem-per-gpu=16gb
-#SBATCH --time=24:00:00
+#SBATCH --time=12:00:00
 #SBATCH --mail-user=m.geraedts1@student.tue.nl
-#SBATCH --mail-type=BEGIN,END
+#SBATCH --mail-type=END
 
 module purge
 module load PyTorch/2.1.2-foss-2023a-CUDA-12.1.1
