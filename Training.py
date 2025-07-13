1990c76d5812240a2d7d1ace21b6050f8b6ab7e1
MaxGeraedts <mailmaxg@gmail.com>
2025-07-13 18:45:40 +0200

    input

--- Training.py
+++ Training.py
@@ -36,7 +36,7 @@ if train == True:
         trainer = Trainer(model, dataset_train,dataset_val,
                         batch_size=32,
                         lr=0.0000001,
-                        epochs=250,
+                        epochs=5,
                         model_name=model_name)
         
         trainer.train_loop()
@@ -51,13 +51,14 @@ if train == True and msg == 'Loaded model':
                                         pre_transform           = pre_transform,
                                         push_forward_step_max   = forward_steps,
                                         bundle_size             = bundle_size,
-                                        model = model) 
+                                        model = model,
+                                        model_ident = model_ident) 
                                         for dataset_type in ["train"]]
     print(f"Training {model_name}_Push")
     trainer = Trainer(model, dataset_train,dataset_val,
                       batch_size=32,
                       lr=0.0000001,
-                      epochs=250,
+                      epochs=5,
                       model_name=f"{dataset_name}_{model_ident}_Push")    
     trainer.train_loop()
     SaveTrainingInfo(dataset_train,trainer)
