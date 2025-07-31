ff4da3f87bc26b6d34245ab0f590bc169a7c8e9b
MaxGeraedts <mailmaxg@gmail.com>
2025-07-31 13:21:39 +0200

    Redone Aggregation Script

--- Aggregate.py
+++ Aggregate.py
@@ -1,10 +1,13 @@
 from Encoding import AggregateRawData, save
 
 
-dataset_name = r"N050_Mono"
+dataset_name = r"2Sphere"
 data_dir = r"D:\TUE\Master\Graduation\Data"
+
+par_data, topology, boundary_conditions, = None, None, None
 Aggregate = AggregateRawData(dataset_name,data_dir)
 par_data = Aggregate.ParticleData()
 topology = Aggregate.TopologyData()
 boundary_conditions = Aggregate.BoundaryConditions()
+
 save(dataset_name,par_data,topology,boundary_conditions)
