e75a7b1b261a83ad68cdc38628fb2c3f03e35c2e
MaxGeraedts <mailmaxg@gmail.com>
2025-08-01 13:08:29 +0200

    Fix directory for HPC

--- Aggregate.py
+++ Aggregate.py
@@ -1,8 +1,11 @@
 from Encoding import AggregateRawData, save
-
+import os
 
 dataset_name = r"2Sphere"
+
 data_dir = r"D:\TUE\Master\Graduation\Data"
+if os.path.exists(data_dir) != True:
+    data_dir = os.path.join("..","Data")
 
 par_data, topology, boundary_conditions, = None, None, None
 Aggregate = AggregateRawData(dataset_name,data_dir)
