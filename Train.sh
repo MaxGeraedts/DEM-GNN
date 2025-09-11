3c1468b7a7898ade448445cbb49c501073b5514f
Max Geraedts <20182319@tue-login002.cluster>
2025-09-11 16:59:43 +0200

    test

--- Train.sh
+++ Train.sh
@@ -5,8 +5,8 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --gpus-per-task=1
-#SBATCH --mem-per-gpu=64gb
-#SBATCH --time=120:00:00
+#SBATCH --mem-per-gpu=64G
+#SBATCH --time=48:00:00
 #SBATCH --mail-user=m.geraedts1@student.tue.nl
 #SBATCH --mail-type=END
 
