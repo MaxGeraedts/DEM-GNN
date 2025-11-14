e46bda1aeba122985da5918a56b6989643c8478b
Max Geraedts <mailmaxg@gmail.com>
2025-11-14 15:44:08 +0100

    Setup emb128 Training

--- Train.sh
+++ Train.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --job-name=emb32_Training
-#SBATCH --output=./Jobs/emb32-%j.log
+#SBATCH --job-name=emb128_Training
+#SBATCH --output=./Jobs/emb128-%j.log
 #SBATCH --partition=be.gpuresearch.q,tue.gpu.q
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
