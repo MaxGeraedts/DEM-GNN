746a3b9ba421af86cd7458bf6fe9a618eeec75b2
MaxGeraedts <mailmaxg@gmail.com>
2025-11-30 15:21:51 +0100

    Setup Training N300

--- Train.sh
+++ Train.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --job-name=2Sphere_Training
-#SBATCH --output=./Jobs/2Sphere_64_redo-%j.log
+#SBATCH --job-name=N300_Training
+#SBATCH --output=./Jobs/N300-%j.log
 #SBATCH --partition=be.gpuresearch.q,tue.gpu.q
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
